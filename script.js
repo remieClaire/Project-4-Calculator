46b46a97a9e1f49364837befa9c96c6548c2437b
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-04-10 18:13:28 -0700

    calculator can now take in multiple digit numbers

--- script.js
+++ script.js
@@ -22,24 +22,26 @@ const subtractOp = document.getElementById('sub-btn');
 const addOp = document.getElementById('add-btn');
 const equalsOp = document.getElementById('equal-btn');
 
-let num1 = 0;
-let num2 = 0;
+let num1 = '';
+let num2 = '';
 
 let storeNum2 = false;
+let inputFin1 = false;
+let inputFin2 = false;
 let currOp = 'none';
 
 // attach click event to buttons
 //num btn functionality
-oneBtn.addEventListener('click', ()=>displayAndStoreNumber(1));
-twoBtn.addEventListener('click', ()=>displayAndStoreNumber(2));
-threeBtn.addEventListener('click', ()=>displayAndStoreNumber(3));
-fourBtn.addEventListener('click', ()=>displayAndStoreNumber(4));
-fiveBtn.addEventListener('click', ()=>displayAndStoreNumber(5));
-sixBtn.addEventListener('click', ()=>displayAndStoreNumber(6));
-sevenBtn.addEventListener('click', ()=>displayAndStoreNumber(7));
-eightBtn.addEventListener('click', ()=>displayAndStoreNumber(8));
-nineBtn.addEventListener('click', ()=>displayAndStoreNumber(9));
-zeroBtn.addEventListener('click', ()=>displayAndStoreNumber(0));
+oneBtn.addEventListener('click', ()=>displayAndStoreNumber('1'));
+twoBtn.addEventListener('click', ()=>displayAndStoreNumber('2'));
+threeBtn.addEventListener('click', ()=>displayAndStoreNumber('3'));
+fourBtn.addEventListener('click', ()=>displayAndStoreNumber('4'));
+fiveBtn.addEventListener('click', ()=>displayAndStoreNumber('5'));
+sixBtn.addEventListener('click', ()=>displayAndStoreNumber('6'));
+sevenBtn.addEventListener('click', ()=>displayAndStoreNumber('7'));
+eightBtn.addEventListener('click', ()=>displayAndStoreNumber('8'));
+nineBtn.addEventListener('click', ()=>displayAndStoreNumber('9'));
+zeroBtn.addEventListener('click', ()=>displayAndStoreNumber('0'));
 
 //op btn functionality
 clearBtn.addEventListener('click', ()=>clearInput());
@@ -50,78 +52,111 @@ addOp.addEventListener('click', ()=>add());
 
 equalsOp.addEventListener('click', ()=>operationsManager());
 
+// store val as a string
+// add to string
+// when another button is pressed, convert it to int
+
+// displays number when number btn is pressed
 function displayAndStoreNumber(num) {
     
-    if (!storeNum2) {
-        num1 = num;
-        storeNum2 = true;
+    if (!storeNum2 && !inputFin1) {
+        num1 += num;
+        screen.textContent = num1;
     }
-    else {
-        num2 = num;
-        storeNum2 = false;
+    else if (storeNum2 && !inputFin2) {
+        num2 += num;
+        screen.textContent = num2;
     }
-    console.log("num1: " + num1);
-    console.log("num2: " + num2);
-    screen.textContent = num;
+    
 }
 
 // sets current operation when corresponding button is pressed
 function divide() {
     currOp = 'divide';
+    inputFin1 = true;
+    storeNum2 = true;
 }
 function multiply() {
     currOp = 'multiply';
+    inputFin1 = true;
+    storeNum2 = true;
 }
 function subtract() {
     currOp = 'subtraction';
+    inputFin1 = true;
+    storeNum2 = true;
 }
 function add() {
     currOp = 'addition';
+    inputFin1 = true;
+    storeNum2 = true;
 }
 
 // calculates results once equals is pressed
 function operationsManager() {
   let result = 0;
 
+  storeNum2 = false;
+  inputFin2 = true;
+  
   if (!storeNum2) {
 
     switch (currOp) {
       case 'divide':
         // calculate result
-        result = num1 / num2;
+        result = parseInt(num1) / parseInt(num2);
 
         // output result to display
         screen.textContent = result;
 
         // reset values
-        num1 = 0;
-        num2 = 0;
+        num1 = '0';
+        num2 = '0';
         currOp = 'none';
+        inputFin1 = false;
+        inputFin2 = false;
         break;
       case 'multiply':
-        result = num1 * num2;
-        screen.textContent = result;
-        console.log("result: " + result);
+        // calculate result
+        result = parseInt(num1) * parseInt(num2);
 
-        num1 = 0;
-        num2 = 0;
+        // output result to display
+        screen.textContent = result;
+
+        // reset values
+        num1 = '0';
+        num2 = '0';
         currOp = 'none';
+        inputFin1 = false;
+        inputFin2 = false;
         break;
       case 'addition':
-        result = num1 + num2;
+        // calculate result
+        result = parseInt(num1) + parseInt(num2);
+
+        // output result to display
         screen.textContent = result;
 
-        num1 = 0;
-        num2 = 0;
+        // reset values
+        num1 = '0';
+        num2 = '0';
         currOp = 'none';
+        inputFin1 = false;
+        inputFin2 = false;
         break;
       case 'subtraction':
-        result = num1 - num2;
+        // calculate result
+        result = parseInt(num1) - parseInt(num2);
+
+        // output result to display
         screen.textContent = result;
 
-        num1 = 0;
-        num2 = 0;
+        // reset values
+        num1 = '0';
+        num2 = '0';
         currOp = 'none';
+        inputFin1 = false;
+        inputFin2 = false;
         break;
       case 'none':
         break;
@@ -130,10 +165,13 @@ function operationsManager() {
 
 }
 
+// clears input & resets
 function clearInput() {
-    num1 = 0;
-    num2 = 0;
+    num1 = '';
+    num2 = '';
     currOp = 'none';
+    inputFin1 = false;
+    inputFin2 = false;
 
     screen.textContent = 0;
 }

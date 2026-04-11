67f3138dbd47ed21cfbe5fea0d5abd9786777938
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-04-10 20:20:30 -0700

    added delete button functionality

--- script.js
+++ script.js
@@ -16,18 +16,26 @@ const zeroBtn = document.getElementById('0-btn');
 
 //op btns
 const clearBtn = document.getElementById('clear-btn');
+const deleteBtn = document.getElementById('del-btn');
+
 const divisionOp = document.getElementById('div-btn');
 const multiplyOp = document.getElementById('mult-btn');
 const subtractOp = document.getElementById('sub-btn');
 const addOp = document.getElementById('add-btn');
 const equalsOp = document.getElementById('equal-btn');
 
+// set number values to strings for multiple digit values
 let num1 = '';
 let num2 = '';
 
+// flag to know when to store input as second value
 let storeNum2 = false;
+
+// flags to know when user input is finished (to allow for multiple digit values)
 let inputFin1 = false;
 let inputFin2 = false;
+
+// keeps track of the current operation
 let currOp = 'none';
 
 // attach click event to buttons
@@ -45,6 +53,8 @@ zeroBtn.addEventListener('click', ()=>displayAndStoreNumber('0'));
 
 //op btn functionality
 clearBtn.addEventListener('click', ()=>clearInput());
+deleteBtn.addEventListener('click', ()=>deleteVal());
+
 divisionOp.addEventListener('click', ()=>divide());
 multiplyOp.addEventListener('click', ()=>multiply());
 subtractOp.addEventListener('click', ()=>subtract());
@@ -162,6 +172,22 @@ function operationsManager() {
         break;
     }
   }
+
+}
+
+// deletes the last inputted number
+function deleteVal() {
+
+    if (!storeNum2 && !inputFin1) {
+        num1 = num1.substring(0, num1.length-1);
+        screen.textContent = num1;
+        console.log("num1: " + num1);
+    }
+    else if (storeNum2 && !inputFin2) {
+        num2 = num2.substring(0, num2.length-1);
+        screen.textContent = num2;
+        console.log("num2: " + num2);
+    }
 
 }
 
